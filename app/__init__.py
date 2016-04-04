b9ee8a97c84b4740e47213078b80652d28b8c4c6
Alex Taylor <alex@subnomo.me>
2016-04-04 12:41:11 -0700

    Have travis move config.py.dist to config.py
    Rather than falling back to config.py.dist in the main application

--- app/__init__.py
+++ app/__init__.py
@@ -1,10 +1,6 @@
 from flask import Flask
 
 app = Flask(__name__)
-
-try:
-    app.config.from_object('config')
-except:
-    app.config.from_pyfile('../config.py.dist')
+app.config.from_object('config')
 
 from app import views  # NOQA flake8 ignore
