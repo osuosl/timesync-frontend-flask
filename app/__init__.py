376041e4b183f7b96c29f838690b6da7480fde3a
Aaron Cohen <cohenaa@osuosl.org>
2016-04-29 13:55:25 -0700

    Initial Redis support

--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,9 @@
 from flask import Flask
+from redis import StrictRedis
 
 app = Flask(__name__)
 app.config.from_object('config')
+
+redis = StrictRedis(host='redis')
 
 from app import views  # NOQA flake8 ignore
