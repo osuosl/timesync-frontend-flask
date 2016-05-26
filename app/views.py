9841277654520849795cb811e706e5e1697d4e1e
Aaron Cohen <cohenaa@osuosl.org>
2016-05-26 14:54:56 -0700

    Fixed admin page

--- app/views.py
+++ app/views.py
@@ -253,23 +253,23 @@ def report():
 @app.route('/admin')
 def admin():
     # Check if logged in first
-    if not isLoggedIn():
+    if not is_logged_in():
         return redirect(url_for('login', next=request.url_rule))
 
     # Check if the user is an admin and deny access if not
-    isAdmin = False
-    user = getUser()
+    is_admin = False
+    user = get_user()
 
     if 'error' in user or 'pymesync error' in user:
         print user
         return "There was an error.", 500
 
     if type(user) is dict:
-        isAdmin = user['site_admin']
+        is_admin = user['site_admin']
     elif type(user) is list:
-        isAdmin = user[0]['site_admin']
+        is_admin = user[0]['site_admin']
 
-    if not isAdmin:
+    if not is_admin:
         return "You cannot access this page.", 401
 
     return render_template('admin.html')
