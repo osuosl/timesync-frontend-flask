695ffeb0115abdc41d3d8918c4f5d445836f68d5
Aaron Cohen <cohenaa@osuosl.org>
2016-06-17 09:13:57 -0700

    Added template and filters for view_projects

--- app/filters.py
+++ app/filters.py
@@ -7,3 +7,14 @@ def hms_filter(seconds):
     h, m = divmod(m, 60)
 
     return "{} h, {} m, {} s".format(h, m, s)
+
+
+@app.template_filter()
+def perm_filter(permissions):
+    perm_strings = []
+
+    for (user, roles) in permissions.itervalues():
+        user_perms = ' '.join(role for role in roles if roles[role])
+        perm_strings.append('{}: {}'.format(user, user_perms))
+
+    return '\n'.join(perm_strings)
